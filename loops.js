23e838754c0606f9580cb12fb3968b09cf9ad4bd
Cassandra Corrales <cmcorrales3@gmail.com>
2018-01-18 03:07:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,11 +18,11 @@ function whileLoop(number) {
   return("done")
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
 
 function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
   do {
     array.pop();
   } while (array.length > -1 && maybeTrue());
